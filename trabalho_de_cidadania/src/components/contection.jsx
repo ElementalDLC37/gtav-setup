4d34957332877adca6bf950ae90aec6784cb86e6
Elemental <lucasantony815@gmail.com>
2023-03-21 00:05:15 -0300

    abc

--- trabalho_de_cidadania/src/components/contection.jsx
+++ trabalho_de_cidadania/src/components/contection.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 function Conn() {
 
   useEffect(() => {
-    axios.get('http://localhost:3000')
+    axios.get('https://trabalho-de-cidadania-4554.vercel.app/')
       .then(response => {
         console.log(response.data);
       })
