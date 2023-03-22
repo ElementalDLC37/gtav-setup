08c38bfbc55f1b52be642f88b1fcd60c7ee05916
Elemental <lucasantony815@gmail.com>
2023-03-22 12:59:49 -0300

    qqq

--- trabalho_de_cidadania/src/components/Apresentation/Apresentation.jsx
+++ trabalho_de_cidadania/src/components/Apresentation/Apresentation.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 
 export function Apresentation() {
     const [isVisible, setIsVisible] = useState(false);
+    const [content, setContent] = useState("Bruno");
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsVisible(true);
@@ -20,7 +22,73 @@ export function Apresentation() {
 
   return (
     <div className="apresentation">
-        aaaa
+        <div className="menu-interface">
+            <div className="header">
+                <h1>Projetos e Valores</h1>
+                <div className="user">
+                    
+                </div>
+            </div>
+            <div className="menu-btn">
+                <button  className={content === "Bruno" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Bruno")}}>Bruno</button>
+                <button className={content === "Rui" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Rui")}}>Rui</button>
+                <button className={content === "Renan" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Renan")}}>Renan</button>
+                <button className={content === "Willian" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Willian")}}>Willian</button>
+                <button className={content === "Kaue" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Kaue")}}>Kaue</button>
+                <button className={content === "Jordan" ? 'btn-clicked' : 'btn'} onClick={() => { setContent("Jordan")}}>Jordan</button>
+            </div>
+            <div className="bottom-interface-content">
+                <MenuButtonSec content={content} />
+            </div>
+        </div>
     </div>
   )
 }
+
+function MenuButtonSec({content}) {
+    const [data, setData] = useState(<></>);
+
+    useEffect(() => {
+        switch (content) {
+            case "Bruno":
+                setData(<>
+                    <button class="btn-sec">Apresentação</button>
+                    <button class="btn-sec">Contextualização</button>
+                </>)
+                break
+            case "Rui":
+                setData(<>
+                    <button class="btn-sec">Exemplos e Projetos</button>
+                </>)
+                break
+            case "Renan":
+                setData(<>
+                    <button class="btn-sec">Explanação</button>
+                </>)
+                break
+            case "Willian":
+                setData(<>
+                    <button class="btn-sec">Participação</button>
+                </>)
+                break
+            case "Kaue":
+                setData(<>
+                    <button class="btn-sec">Cidadania e Direitos</button>
+                </>)
+                break 
+            case "Jordan":
+                setData(<>
+                    <button class="btn-sec">Conclusão</button>
+                </>)
+                break  
+        }
+    }, [content])
+    
+    return (
+        <div className="menu-btn-sec">
+            {data}
+        </div>
+    )
+}
+
+
